b1bfb243ff5768d180131806e28a5fa129107428
Jonathan Jibikilayi <jonathan.jibikilayi@esprit.tn>
2021-04-17 14:24:42 +0100

    je suis la

---
+++ src/main/java/tn/esprit/sring/ConsommiTounsiLeaders2021Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.sring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsommiTounsiLeaders2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsommiTounsiLeaders2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/sring/ConsommiTounsiLeaders2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.sring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsommiTounsiLeaders2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
